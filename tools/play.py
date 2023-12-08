ded1c48458574c5bc190728a40052a7258d0305f
flonix8 <flstanek@googlemail.com>
2023-12-08 17:07:55 +0100

    play.py: Fix Ctrl-C handling; Add loop mode

--- tools/play.py
+++ tools/play.py
@@ -35,6 +35,7 @@ if __name__ == '__main__':
 
     arg_parser = argparse.ArgumentParser()
     arg_parser.add_argument('-o', '--input-file', type=str, required=True)
+    arg_parser.add_argument('-l', '--loop', action='store_true')
     arg_parser.add_argument('--redis-host', type=str, default='localhost')
     arg_parser.add_argument('--redis-port', type=int, default=6379)
     args = arg_parser.parse_args()
@@ -54,23 +55,27 @@ if __name__ == '__main__':
 
     publish = RedisPublisher(REDIS_HOST, REDIS_PORT)
 
-    start_time = time.time()
-
     with publish, open(args.input_file, 'r') as input_file:
-        message_reader = read_messages(input_file)
+        while True:
+            message_reader = read_messages(input_file)
 
-        start_message = next(message_reader)
-        dump_meta = DumpMeta.model_validate_json(start_message)
-        print(f'Starting playback from file {args.input_file} containing streams {dump_meta.recorded_streams}')
+            playback_start = time.time()
+            start_message = next(message_reader)
+            dump_meta = DumpMeta.model_validate_json(start_message)
+            print(f'Starting playback from file {args.input_file} containing streams {dump_meta.recorded_streams}')
 
-        while not stop_event.is_set():
             for message in message_reader:
                 event = Event.model_validate_json(message)
 
-                wait_until(start_time, dump_meta.start_time, event.meta.record_time)
+                wait_until(playback_start, dump_meta.start_time, event.meta.record_time)
 
                 publish(event.meta.source_stream, pybase64.standard_b64decode(event.data_b64))
-                
-            break
 
+                if stop_event.is_set():
+                    break
+            
+            if not args.loop or stop_event.is_set():
+                break
+            else:
+                input_file.seek(0)
             
