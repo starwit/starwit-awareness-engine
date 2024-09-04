33d9e2c98abad19a05e63590e8af321b19150e30
flonix8 <flstanek@googlemail.com>
2024-09-04 19:23:27 +0200

    Tools: Add some more comfort to plot-trajectories.py

--- tools/cameras/plot-trajectories.py
+++ tools/cameras/plot-trajectories.py
@@ -10,6 +10,7 @@ import numpy as np
 import psycopg
 from palettable.colorbrewer.qualitative import Set1_9
 from psycopg.rows import class_row
+from tqdm import tqdm
 
 CLS_CMAP = Set1_9.colors
 
@@ -30,7 +31,7 @@ class DetectionRow:
     longitude: float
 
 
-def fetch_data(start_time: datetime, length: dt.timedelta, camera_id: str) -> List[DetectionRow]:
+def fetch_data(start_time: datetime, end_time: datetime, camera_id: str) -> List[DetectionRow]:
     conn_params = {
         'dbname': 'saebackend',
         'user': 'saebackend',
@@ -41,11 +42,18 @@ def fetch_data(start_time: datetime, length: dt.timedelta, camera_id: str) -> Li
     }
 
     try:
+        print('Connecting to database')
         with psycopg.connect(**conn_params) as conn:
             with conn.cursor() as cur:
-                query = "SELECT * FROM detection WHERE capture_ts >= %s and capture_ts <= %s and camera_id = %s;"
-                cur.execute(query, (start_time.isoformat(), start_time + length, camera_id))
-                rows = cur.fetchall()
+                query = "SELECT * FROM detection WHERE capture_ts >= %s AND capture_ts <= %s AND camera_id = %s ORDER BY capture_ts ASC;"
+                print(f'Running query (camera: {camera_id}, start: {start_time.isoformat(timespec='seconds')}, end: {end_time.isoformat(timespec='seconds')})')
+                cur.execute(query, (start_time.isoformat(), end_time, camera_id))
+                rows = []
+                print('Loading result')
+                with tqdm(unit=' rows') as pbar:
+                    while (len(records := cur.fetchmany(10000)) > 0):
+                        pbar.update(len(records))
+                        rows += records
                 return rows
 
     except psycopg.DatabaseError as e:
@@ -69,31 +77,36 @@ def draw_trajectories(img, detections: List[DetectionRow]):
         preceding_points[det.object_id] = new_point
 
 def draw_line(img, pt1: Tuple[float], pt2: Tuple[float], class_id: int):
-    cv2.line(img, (int(pt1[0] * img.shape[1]), int(pt1[1] * img.shape[0])), (int(pt2[0] * img.shape[1]), int(pt2[1] * img.shape[0])), color=get_color(class_id), thickness=2)
+    cv2.line(img, (int(pt1[0] * img.shape[1]), int(pt1[1] * img.shape[0])), (int(pt2[0] * img.shape[1]), int(pt2[1] * img.shape[0])), color=get_color(class_id), thickness=2, lineType=cv2.LINE_AA)
 
 if __name__ == "__main__":
     arg_parser = argparse.ArgumentParser()
     arg_parser.add_argument('-c', '--camera-id', type=str, required=True)
     arg_parser.add_argument('-i', '--image-file', type=Path)
-    arg_parser.add_argument('-s', '--start-time', type=dt.datetime.fromisoformat, default=datetime.now() - dt.timedelta(hours=1), help='Data segment start time (in isoformat)')
+    arg_parser.add_argument('-s', '--start-time', type=dt.datetime.fromisoformat, help='Data segment start time (in isoformat)')
     arg_parser.add_argument('-l', '--length', type=lambda s: dt.timedelta(seconds=int(s)), default=dt.timedelta(minutes=10), help='Length of data segment to load [s]')
 
     args = arg_parser.parse_args()
 
+    if args.start_time is None:
+        start_time = dt.datetime.now(tz=dt.UTC) - args.length
+    else:
+        start_time = args.start_time
+
+    end_time = start_time + args.length
+
     if args.image_file is None:
         image = np.ones((1080, 1920, 3), dtype=np.uint8) * 127
-        image_file = Path('.')
     else:
         image = cv2.imread(args.image_file, cv2.IMREAD_COLOR)
-        image_file = args.image_file
 
     annotated_image = image.copy()
 
-    output_file = image_file.parent / f'{args.camera_id}_{args.start_time}_{args.length.seconds}s.jpg'
+    output_file = Path('.') / f'{args.camera_id}_{args.start_time}_{args.length.seconds}s.jpg'
 
-    print('Loading data...')
+    print('Loading data')
 
-    rows = fetch_data(args.start_time, args.length, args.camera_id)
+    rows = fetch_data(start_time, end_time, args.camera_id)
 
     print(f'Retrieved {len(rows)} data points from DB')
 
@@ -102,7 +115,7 @@ if __name__ == "__main__":
     if args.image_file is None:
         output_image = annotated_image
     else:
-        alpha = 0.75
+        alpha = 0.80
         output_image = cv2.addWeighted(annotated_image, alpha, image, 1 - alpha, 0)
 
     cv2.imwrite(output_file, output_image, [cv2.IMWRITE_JPEG_QUALITY, 90])
