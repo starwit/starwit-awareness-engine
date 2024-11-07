5b48a3c24f9fb06503f0691dec0843ea72c5bad3
flonix8 <flstanek@googlemail.com>
2024-11-07 17:14:41 +0100

    Tools: Add video_snapshot script

--- tools/misc/video_snapshots.sh
+++ tools/misc/video_snapshots.sh
@@ -3,4 +3,25 @@
 # This scripts purpose is to continuously collect snapshots from video streams
 # Please adjust fps and file name padding to your liking
 
-ffmpeg -rtsp_transport tcp -i rtsp://localhost:8554/video-stream -vf fps=1/10 -q:v 1 out%04d.jpg
+#!/bin/bash
+
+SNAPSHOT_INTERVAL_S=600
+
+# Define video sources
+sources=(
+        "source1"
+        "source2"
+)  
+
+# Replace these with actual video source names/URLs
+cam_ids=(
+        "source-name1"
+        "source-name2"
+)
+
+# Start recording each video source
+for i in "${!sources[@]}"; do
+    ffmpeg -nostdin -rtsp_transport tcp -i "${sources[$i]}" -vf fps=1/${SNAPSHOT_INTERVAL_S} -q:v 1 "file:snapshots/${cam_ids[$i]}_${RANDOM}_%05d.jpg" &
+done
+
+wait
