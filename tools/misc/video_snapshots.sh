8f2d70e63d079db0ce7929433acaddd4e2286bee
flonix8 <flstanek@googlemail.com>
2024-11-12 10:59:37 +0100

    Tools: add image masking feature to video_snapshots.sh

--- tools/misc/video_snapshots.sh
+++ tools/misc/video_snapshots.sh
@@ -2,21 +2,20 @@
 
 # This scripts purpose is to continuously collect snapshots from video streams
 # Please adjust fps and file name padding to your liking
-
-#!/bin/bash
+# If there is an image file "${cam_id}_mask.png" it will be used to mask the image (black=blacken; white=keep)
 
 SNAPSHOT_INTERVAL_S=600
 SNAPSHOT_DIRECTORY=./snapshots
 
 mkdir -p ${SNAPSHOT_DIRECTORY}
 
-# Define video sources
+# Define video sources (e.g. URL)
 sources=(
         "source1"
         "source2"
 )  
 
-# Replace these with actual video source names/URLs
+# These will be used to prefix the corresponding output files
 cam_ids=(
         "source-name1"
         "source-name2"
@@ -24,7 +23,11 @@ cam_ids=(
 
 # Start recording each video source
 for i in "${!sources[@]}"; do
-    ffmpeg -nostdin -rtsp_transport tcp -i "${sources[$i]}" -vf fps=1/${SNAPSHOT_INTERVAL_S} -q:v 1 "file:${SNAPSHOT_DIRECTORY}/${cam_ids[$i]}_${RANDOM}_%05d.jpg" &
+    if [ -f "${cam_ids[$i]}_mask.png" ]; do
+        ffmpeg -nostdin -rtsp_transport tcp -i "${sources[$i]}" -i "${cam_ids[$i]}_mask.png" -filter_complex "[1:v]colorkey=white[ckout];[0:v][ckout]overlay[out]" -map "[out]" -vf fps=1/${SNAPSHOT_INTERVAL_S} -q:v 1 "file:${SNAPSHOT_DIRECTORY}/${cam_ids[$i]}_${RANDOM}_%05d.jpg" &
+    else
+        ffmpeg -nostdin -rtsp_transport tcp -i "${sources[$i]}" -vf fps=1/${SNAPSHOT_INTERVAL_S} -q:v 1 "file:${SNAPSHOT_DIRECTORY}/${cam_ids[$i]}_${RANDOM}_%05d.jpg" &
+    fi
 done
 
 wait
