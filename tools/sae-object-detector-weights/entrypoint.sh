49508c23cba60845ee9978d47f50a80764213698
flonix8 <flstanek@googlemail.com>
2025-09-12 12:26:43 +0200

    sae-object-detector-weights: Also copy directories (needed for openvino)

--- tools/sae-object-detector-weights/entrypoint.sh
+++ tools/sae-object-detector-weights/entrypoint.sh
@@ -2,4 +2,4 @@
 
 # This scripts purpose is to copy all the files in /weights into /weights_target
 
-cp /weights/* /weights_target/ && echo -e "Successfully copied weights:\n$(ls /weights)"
+cp -r /weights/* /weights_target/ && echo -e "Successfully copied weights:\n$(ls /weights)"
